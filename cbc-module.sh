3ac7aea134cf21c90d9ddb443b5a5b8a22979f3a
iop098321qwe <iop098321qwe@pm.me>
2026-01-06 13:18:56 -0700

    test

--- cbc-module.sh
+++ cbc-module.sh
@@ -35,3 +35,33 @@ function verg() {
     fi
   fi
 }
+
+
+function vergg() {
+  gum style \
+    --border rounded \
+    --border-foreground "#b4befe" \
+    --margin "1 0" \
+    --padding "0 2" \
+    "PREVIEWING NEXT VERSION"
+  if ! npx commit-and-tag-version --dry-run --skip.commit --skip.tag | delta; then
+    return 1
+  fi
+
+  if ! gum confirm "Proceed with commit-and-tag-version?"; then
+    return 0
+  fi
+
+  if npx commit-and-tag-version; then
+    if gum confirm "Push commits and tags?"; then
+      if git push && git push --tags; then
+        local latest_tag
+        latest_tag=$(git describe --tags --abbrev=0)
+
+        if gh release create "$latest_tag" --notes-file CHANGELOG.md -d; then
+          gh browse -r
+        fi
+      fi
+    fi
+  fi
+}
